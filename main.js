d731019533513babd90e5f5c3ab087fbacbbc0e5
LorenzH1 <72896156+LorenzH1@users.noreply.github.com>
2021-02-08 18:28:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie"){
+        console.log("Taking selfie");
+        speak();
+    }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "taking the selfie in 5 seconds";
+    var utter_this = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
+}
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image"src="'+ data_uri +'">'
+    });
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width: 360,
+    height: 350,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
